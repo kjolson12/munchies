973e7c70b1ac479b36b7d0a84db739f3ee28b118
kjolson12 <kobaseball02@gmail.com>
2021-01-06 19:51:10 -0800

    added search on sort button click

--- src/components/SortBy.js
+++ src/components/SortBy.js
@@ -4,16 +4,20 @@ import { Button } from 'semantic-ui-react';
 import './SortBy.css';
 
 import { connect } from 'react-redux';
-import { setSortBy } from '../actions/index';
+import { setSortBy, fetchBusinesses } from '../actions/index';
 
-const SortBy = ({ setSortBy }) => {
+const SortBy = ({ setSortBy, fetchBusinesses, sortBy, searchTerm, searchLocation }) => {
     const [activeButton, setActiveButton] = useState('');
     
     const renderActiveButton = button => {
         if (button === activeButton) {
             return 'activeButton';
-        }
-    }
+        };
+    };
+
+    const search = () => {
+        fetchBusinesses(searchTerm, searchLocation, sortBy);
+    };
 
     return (
         <div className='btnGroup'>
@@ -22,6 +26,7 @@ const SortBy = ({ setSortBy }) => {
                     onClick={() => {
                         setSortBy('best_match');
                         setActiveButton('best_match');
+                        search();
                     }}
                     id={renderActiveButton('best_match')}
                 >
@@ -31,6 +36,7 @@ const SortBy = ({ setSortBy }) => {
                     onClick={() => {
                         setSortBy('review_count');
                         setActiveButton('review_count');
+                        search();
                     }}
                     id={renderActiveButton('review_count')}
                 >
@@ -40,6 +46,7 @@ const SortBy = ({ setSortBy }) => {
                     onClick={() => {
                         setSortBy('rating');
                         setActiveButton('rating');
+                        search();
                     }}
                     id={renderActiveButton('rating')}
                 >
@@ -50,4 +57,12 @@ const SortBy = ({ setSortBy }) => {
     );
 };
 
-export default connect(null, { setSortBy })(SortBy);
+const mapStateToProps = state => {
+    return {
+        sortBy: state.sortBy,
+        searchTerm: state.searchTerm,
+        searchLocation: state.searchLocation
+    }
+}
+
+export default connect(mapStateToProps, { setSortBy, fetchBusinesses })(SortBy);
