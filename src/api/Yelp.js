1140dae6a4bb5b0723ff46da4b8be5d941bb8017
kjolson12 <kobaseball02@gmail.com>
2021-02-20 10:22:16 -0800

    using own cors anywhere

--- src/api/Yelp.js
+++ src/api/Yelp.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiKey = 'eGabxjjmOWmymwl6LhEQ3fe_1w7M7qd0xhsxL3ISAHp_RBFphFAmw_Ab1lujnavUbjEHE6_aLnu1VMkVU4231kn2DheWXqNrhRPmxlLsFNiPoxScokNoQJmBBhYCX3Yx';
 
 export default axios.create({
-    baseURL: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3`,
+    baseURL: `https://cors-anywhere-ecru.vercel.app/https://api.yelp.com/v3`,
     headers: {
         Authorization: `Bearer ${apiKey}`
     }
